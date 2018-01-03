4e398672c88280d5e6f46cdef1a9ffae8a35a85f
Sergey <sergei_smirnov@epam.com>
2018-01-03 23:37:07 +0300

    Revert "comment from master"
    This reverts commit 9464603890518db6ca2a4b8c609c4975cd64d0ff.

--- sample4/src/com/epam/pack1/First.java
+++ sample4/src/com/epam/pack1/First.java
@@ -10,8 +10,7 @@ import static com.epam.pack2.Second.STATIC_STRING_VARIABLE;
                 /*com.epam.pack2.*/Second secondPack2 = new /*com.epam.pack2.*/Second();
                 secondPack1.sayHello();
                 secondPack2.sayHello();
-//comment from new branch
-                
+
 
                 // Checking static method:
                // com.epam.pack1_sample6.First.sayHelloStaticMethod();
